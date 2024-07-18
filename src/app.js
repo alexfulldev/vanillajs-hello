81d1ec9ca1afa7d6d788850067550b2d6bb845e5
alexfulldev <166410130+alexfulldev@users.noreply.github.com>
2024-07-18 18:48:41 +0000

    acabado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { left } from "@popperjs/core";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  function generateRandomNumber() {
+    let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+    let randomNumber = Math.floor(Math.random() * numbers.length);
+    return numbers[randomNumber];
+  }
+
+  function generateRandomSuit() {
+    let suit = ["♦", "♥", "♠", "♣"];
+    let randomSuit = Math.floor(Math.random() * suit.length);
+    return suit[randomSuit];
+  }
+
+  document.querySelector(".card-number").innerHTML = generateRandomNumber();
+  let topSuit = generateRandomSuit();
+  document.querySelector(".card-suit-top").innerHTML = topSuit;
+  document.querySelector(".card-suit-bottom").innerHTML = topSuit;
 };
